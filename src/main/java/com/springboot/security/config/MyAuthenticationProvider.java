865157d7663857b98cfa9a3b894a52ba31ab04fa
chenjunnan <m18460365733_1@163.com>
2020-11-16 11:45:18 +0800

    数据库数据

--- src/main/java/com/springboot/security/config/MyAuthenticationProvider.java
+++ src/main/java/com/springboot/security/config/MyAuthenticationProvider.java
@@ -48,4 +48,76 @@ public class MyAuthenticationProvider implements AuthenticationProvider {
     public boolean supports(Class<?> aClass) {
         return true;
     }
+
+    /**
+     * 数据库
+     *
+     * CREATE TABLE IF NOT EXISTS USER (
+     *   id       INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+     *   name     VARCHAR(10),
+     *   password VARCHAR(50)
+     * )
+     *   CHARSET utf8;
+     *
+     * CREATE TABLE IF NOT EXISTS ROLE (
+     *   id   INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+     *   name VARCHAR(10)
+     * )
+     *   CHARSET utf8;
+     *
+     * CREATE TABLE IF NOT EXISTS ROLE_USER (
+     *   user_id INT,
+     *   role_id INT
+     * )
+     *   CHARSET utf8;
+     *
+     * # 防止项目启动，出现重复插入而报错
+     * INSERT INTO `ROLE` (`id`, `name`) SELECT
+     *                                     '1',
+     *                                     'ROLE_ADMIN'
+     *                                   FROM dual
+     *                                   WHERE NOT exists(SELECT id
+     *                                                    FROM `ROLE`
+     *                                                    WHERE id = '1');
+     * INSERT INTO `ROLE` (`id`, `name`) SELECT
+     *                                     '2',
+     *                                     'ROLE_USER'
+     *                                   FROM dual
+     *                                   WHERE NOT exists(SELECT id
+     *                                                    FROM `ROLE`
+     *                                                    WHERE id = '2');
+     *
+     * INSERT INTO `USER` (`id`, `password`, `name`) SELECT
+     *                                                 '1',
+     *                                                 'root',
+     *                                                 'root'
+     *                                               FROM dual
+     *                                               WHERE NOT exists(SELECT id
+     *                                                                FROM `USER`
+     *                                                                WHERE id = '1');
+     * INSERT INTO `USER` (`id`, `password`, `name`) SELECT
+     *                                                 '2',
+     *                                                 'yiaz',
+     *                                                 'yiaz'
+     *                                               FROM dual
+     *                                               WHERE NOT exists(SELECT id
+     *                                                                FROM `USER`
+     *                                                                WHERE id = '2');
+     *
+     *
+     * INSERT INTO `ROLE_USER` (`user_id`, `role_id`) SELECT
+     *                                                  '1',
+     *                                                  '1'
+     *                                                FROM dual
+     *                                                WHERE NOT exists(SELECT user_id
+     *                                                                 FROM `ROLE_USER`
+     *                                                                 WHERE user_id = '1');
+     * INSERT INTO `ROLE_USER` (`user_id`, `role_id`) SELECT
+     *                                                  '2',
+     *                                                  '2'
+     *                                                FROM dual
+     *                                                WHERE NOT exists(SELECT user_id
+     *                                                                 FROM `ROLE_USER`
+     *                                                                 WHERE user_id = '2');
+     */
 }
